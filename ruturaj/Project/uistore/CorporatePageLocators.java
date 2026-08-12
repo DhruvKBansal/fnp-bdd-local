1277004ac977b312a70864fc97dad01bd44d273d
Ruturaj Bhoite <150769355+Ranazaur@users.noreply.github.com>
2026-08-13 00:48:35 +0530

    Create CorporatePageLocators.java

---
+++ ruturaj/Project/uistore/CorporatePageLocators.java
@@ -0,0 +1,7 @@
+package uistore;
+
+import org.openqa.selenium.By;
+
+public class CorporatePageLocators {
+    public static By requestCallback = By.xpath("//button[text()='Request a call back']");
+}
