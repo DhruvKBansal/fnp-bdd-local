d138fb13b39f9fbf64cd00c5dd7e2767f171221a
Benedict Moncy <bennyforcode@gmail.com>
2026-08-13 00:45:07 +0530

    Create ProductListingLocators.java

---
+++ benny/uistore/ProductListingLocators.java
@@ -0,0 +1,10 @@
+package uistore;
+
+import org.openqa.selenium.By;
+
+public class ProductListingLocators {
+
+    public static By pageHeading = By.xpath("//h1[@class]");
+    public static By firstProduct = By.xpath("//div[@itemprop='name']");
+
+}
